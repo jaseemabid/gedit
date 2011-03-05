3fd4f0216ca01ccbac6cd4eb3674f474997ea0e0
Cosimo Cecchi <cosimoc@gnome.org>
2011-03-05 15:40:44 -0500

    window: set the primary toolbar style class on the toolbar

--- gedit/gedit-window.c
+++ gedit/gedit-window.c
@@ -1685,6 +1685,9 @@ create_menu_bar_and_toolbar (GeditWindow *window,
 			    0);
 
 	window->priv->toolbar = gtk_ui_manager_get_widget (manager, "/ToolBar");
+	gtk_style_context_add_class (gtk_widget_get_style_context (window->priv->toolbar),
+				     GTK_STYLE_CLASS_PRIMARY_TOOLBAR);
+
 	gtk_box_pack_start (GTK_BOX (main_box),
 			    window->priv->toolbar,
 			    FALSE,
