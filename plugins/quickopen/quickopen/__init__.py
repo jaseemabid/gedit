8f3b24117ef83e53d52b792bca7b79a68d4758e9
Ignacio Casal Quinteiro <icq@gnome.org>
2011-07-03 13:37:31 +0200

    quickopen: check that the filebrowser has registered the message bus
    This avoids a warning when trying to use quickopen without filebrowser

--- plugins/quickopen/quickopen/__init__.py
+++ plugins/quickopen/quickopen/__init__.py
@@ -92,7 +92,7 @@ class QuickOpenPlugin(GObject.Object, Gedit.WindowActivatable):
         # File browser root directory
         bus = self.window.get_message_bus()
 
-        try:
+        if bus.is_registered('/plugins/filebrowser', 'get_root'):
             msg = bus.send_sync('/plugins/filebrowser', 'get_root')
 
             if msg:
@@ -100,9 +100,6 @@ class QuickOpenPlugin(GObject.Object, Gedit.WindowActivatable):
 
                 if gfile and gfile.is_native():
                     paths.append(gfile)
-
-        except StandardError as e:
-            print e
 
         # Recent documents
         paths.append(RecentDocumentsDirectory())
